ca9fa4328fd6c730b371143e1621ca7037be3924
Lehlohonolo Monareng <43777245+ClaudeMonareng@users.noreply.github.com>
2025-08-31 18:38:24 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 
 // TOGGLE NAVBAR
 
-const menuBtn = document.querySelector(".menu-btn");
-const navigation = document.querySelector(".navigation");
+  const menuBtn = document.querySelector(".nav-menu-btn");
+  const closeBtn = document.querySelector(".nav-close-btn");
+  const navigation = document.querySelector(".navigation");
 
 menuBtn.addEventListener("click", () => {
     menuBtn.classList.toggle("active");
     navigation.classList.toggle("active");
 });
+
+closeBtn.addEventListener("click", () => {
+
+    navigation.classList.remove("active");
+})
 
 // now, we will fix navigation issue...menu close after clicked any link
 
